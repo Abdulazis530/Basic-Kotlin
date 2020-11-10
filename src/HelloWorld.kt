716da11d8087aa6413ed25e197f12f2d1d4fd56a
azis <abdlazis530@gmail.com>
2020-11-10 20:55:56 +0700

    adding typedata file

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,4 +1,26 @@
 fun main() {
+    // type data kotlin
+    //number : integer and float
+    var age : Byte =30;
+    var height :Int =232
+    var distance:Short=2000
+    var balance:Long=100000000L
 
-    v1
+    var thisIsFloat :Float =22.54F
+    var thisIsDouble:Double=22.54221312
+
+    println(age > height)
+    println(thisIsFloat)
+    println(thisIsDouble)
+
+    //Conversion
+    var number :Int= 100
+
+    var byte: Byte=number.toByte()
+    var short: Short=number.toShort()
+
+    println(byte)
+    println(short)
+
+
 }
---
+++ src/TypeData.kt
@@ -0,0 +1,7 @@
+fun main(){
+    val char1: Char ='E'
+//    var char2 ='L'
+//    var char3 ='M'
+
+    println(char1)
+}
