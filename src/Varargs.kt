fa1e3e5116052a619262e66d2501fe82154cb996
azis <abdlazis530@gmail.com>
2020-11-13 16:54:34 +0700

    learning varags

---
+++ src/Varargs.kt
@@ -0,0 +1,32 @@
+/*Parameter yang berada diposisi akhir, memiliki kemampuan menjadi varags*/
+/*Varags artinya datanya bisa menerima lebih dari satu input, atau anggap saja semacam array*/
+/*Bedanya dengan parameter biasa dengan tipe data Array adalah:*/
+/*1.Jika parameter tipe Array, kita wajib membuat array terlebih dahulu sebelum mengirimkan ke function*/
+/*2.Jika parameter menggunakan varrags, kita bisa langsung mengirim datanya, jika lebih dari satu cukup gunakan tanda koma*/
+
+fun hitungTotalArray(values: Array<Int>): Int{
+    var total=0
+    for(value in values){
+        total+=value
+    }
+    return total
+}
+
+fun hitungTotalVarags(vararg values: Int): Int{
+    var total=0
+    for(value in values){
+        total+=value
+    }
+    return total
+}
+
+fun main() {
+    /*dengan array biasa*/
+    var valuesArray= arrayOf(10,10,10,10)
+    var result=hitungTotalArray(valuesArray)
+
+    /*dengan varags biasa*/
+    var resultVarags=hitungTotalVarags(10,10,10)
+    println(result)
+    println(resultVarags)
+}
